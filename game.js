841739ece0514ed608ec8bea02c541fee34f1a33
Ankoopa <dmmdasalla@addu.edu.ph>
2020-02-15 15:06:59 +0800

    Added basic cycles

--- game.js
+++ game.js
@@ -1,12 +1,26 @@
 var config = {
     type: Phaser.WEBGL,
-    width: 800,
-    height: 600,
-    backgroundColor: '#000000',
+    width: 1250,
+    height: 480,
+    backgroundColor: '#2975f0',
     parent: 'phaser-example',
     scene: {
         preload: preload,
         create: create,
         update: update
     }
+};
+
+var game = new Phaser.Game(config);
+
+function preload(){
+    this.load.image('player', 'assets/placeholder-player.png');
+}
+
+function create(){
+
+}
+
+function update(){
+
 };
